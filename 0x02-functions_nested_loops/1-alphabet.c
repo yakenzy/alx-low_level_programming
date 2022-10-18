27c0fcc004cee1f6c3ce33ba6ce68f560b6ade40
yakenzy <yakubohiaka@gmail.com>
2022-10-18 10:01:40 +0100

    fixing error

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n);
+}
