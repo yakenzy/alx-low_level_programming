93102dee1a39a9e53b22f1812b17eeecfaf68e7f
yakenzy <yakubohiaka@gmail.com>
2022-10-19 14:25:39 +0100

    file fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
  * main - main function
  * Description: Print _putchar
  * Return: 0
-=======
  *main - main function
  *Description: Print _putchar
  *Return: 0
->>>>>>> 2c1a1c659f152ed210be752191cda323f3508bad
 */
 int main(void)
 {
