ccbb1b69a03f5fdb42f959da2d970e67713efde3
yakenzy <yakubohiaka@gmail.com>
2022-10-25 17:21:33 +0100

    solutions

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other strig character
+ * @str: char array type
+ */
+
+void puts2(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array
+ * @a: int type pointer
+ * @n: int type
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0: n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
