5d154cb6276e7169aac225f18dfebb541fafff12
yakenzy <yakubohiaka@gmail.com>
2022-10-25 16:22:58 +0100

    task 3 solution

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string and a newline
+ * @str: char type
+ */
+void _puts(char *str)
+{
+	char c[] = *str;
+
+	_putchar(c);
+	_putchar("\n");
+}
