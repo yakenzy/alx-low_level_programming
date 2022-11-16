41f9bf524d0543ffbc9aeb235c3bac4088252aa0
yakenzy <yakubohiaka@gmail.com>
2022-11-16 10:10:22 +0100

    remaining solutions

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the compiled file
+ * @Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
